e2f59eae7075c9672fb7f4005bf2aa4691e5d939
fratur <francescoturco2795@gmail.com>
2025-07-01 22:35:06 +0200

    guest-list

--- src/CheckGuest.java
+++ src/CheckGuest.java
@@ -1,5 +1,20 @@
+
+
+/*
+State lavorando col servizio di sicurezza dei Ferragnez e il vostro compito è di assicurarvi che accedano alla festa solo gli invitati presenti sulla lista
+- creare e inizializzare l’array contenente i nomi degli invitati
+- chiedere all’utente come si chiama
+- verificare che il nome sia presente nella lista
+- lasciarlo entrare o rispedirlo cortesemente da dove è venuto 
+ */
+
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) throws Exception {
         
+        Scanner scan = new Scanner(System.in);
+
+        String[] listaInvitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax","Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis","Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
     }
 }
